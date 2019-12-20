ed1d97cabd2a51088178eed62b838cde23abdca3
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-19 19:21:40 -0700

    first commit

---
+++ NYTCode.js
@@ -0,0 +1,8 @@
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=1rE4ms03B4LVmeLbHYVuXALfJxcDtiiT"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (reponse) {
+
+})
